8b6563d679ec3a2fc571cc6dfe152bde9d612cb0
Monisha <monishakodhandapani@gmail.com>
2025-11-09 11:18:33 +0530

    add loops

---
+++ Loops.java
@@ -0,0 +1,23 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("For loop iteration: " + i);
+        }
+
+        // while loop
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("While loop iteration: " + j);
+            j++;
+        }
+
+        // do-while loop
+        int k = 1;
+        do {
+            System.out.println("Do-while loop iteration: " + k);
+            k++;
+        } while (k <= 5);
+    }
+}
+
